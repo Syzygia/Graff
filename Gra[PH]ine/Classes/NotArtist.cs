e3b4f46fdfdb26e49318644e78a92efa98a1e345
Snetkov Danila <snetkov.da@.students.dvfu.ru>
2019-01-27 16:50:27 +1000

    Add EllipsTool , now u can draw Ellips

--- Gra[PH]ine/Classes/NotArtist.cs
+++ Gra[PH]ine/Classes/NotArtist.cs
@@ -9,9 +9,9 @@ namespace Gra_PH_ine.Classes
     {
         public static List<Figure> Figures = new List<Figure>();
 
-        public static List<Gra_PH_ine.Classes.Tools.Tool> Tools = new List<Tool>() { new LineTool(),new RectTool()};
+        public static List<Gra_PH_ine.Classes.Tools.Tool> Tools = new List<Tool>() { new LineTool(),new RectTool(),new EllipseTool()};
 
-        public static Tool SelectedTool = Tools[1];
+        public static Tool SelectedTool = Tools[2];
 
         public static FgtHost FgtHost = new FgtHost();
     }
