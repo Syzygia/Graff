e3dd0a4c24cc40e5face6b1cc9475565c24e0b52
Snetkov Danila <snetkov.da@.students.dvfu.ru>
2019-01-27 16:43:54 +1000

    Add recttool , now u can draw rectangle

--- Gra[PH]ine/Classes/NotArtist.cs
+++ Gra[PH]ine/Classes/NotArtist.cs
@@ -9,9 +9,9 @@ namespace Gra_PH_ine.Classes
     {
         public static List<Figure> Figures = new List<Figure>();
 
-        public static List<Gra_PH_ine.Classes.Tools.Tool> Tools = new List<Tool>() { new LineTool()};
+        public static List<Gra_PH_ine.Classes.Tools.Tool> Tools = new List<Tool>() { new LineTool(),new RectTool()};
 
-        public static Tool SelectedTool = Tools[0];
+        public static Tool SelectedTool = Tools[1];
 
         public static FgtHost FgtHost = new FgtHost();
     }
---
+++ Gra[PH]ine/Classes/Tools/RectTool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Gra_PH_ine.Classes.Figures;
+using Gra_PH_ine.Classes.Tools;
+
+namespace Gra_PH_ine.Classes.Tools
+{
+    class RectTool:Tool
+    {
+        public override void MouseDown(Point p)
+        {
+            NotArtist.Figures.Add(new Rectangle(p));
+        }
+
+        public override void MouseMove(Point p)
+        {
+            NotArtist.Figures[NotArtist.Figures.Count - 1].AddPoint(p);
+        }
+
+        public override void MouseUp(Point p)
+        {
+            // base.MouseUp(p);
+        }
+    }
+}
