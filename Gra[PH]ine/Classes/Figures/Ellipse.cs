d8abada83d9029784a16141b24aa164a33619507
Snetkov Danila <snetkov.da@.students.dvfu.ru>
2019-01-27 16:46:28 +1000

    Add Elipse class (figure)

---
+++ Gra[PH]ine/Classes/Figures/Ellipse.cs
@@ -0,0 +1,32 @@
+﻿using Gra_PH_ine.Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Gra_PH_ine.Classes.Figures
+{
+   
+        public class Ellipse : Figure
+        {
+            public Ellipse(Point p) : base(p)
+            {
+
+            }
+            public override void AddPoint(Point p)
+            {
+                points[1] = p;
+            }
+            public override void Draw(DrawingContext dc)
+            {
+                var space = Vector.Divide(Point.Subtract(points[0], points[1]), 2.0);
+                var center = Point.Add(points[1], space);
+
+                dc.DrawEllipse(new SolidColorBrush(Colors.Transparent), new Pen(Brushes.Black, 4), center, space.X, space.Y);
+            }
+        }
+    }
+
